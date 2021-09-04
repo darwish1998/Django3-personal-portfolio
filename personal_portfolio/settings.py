b66714b9400865f584a52f2393552dc041df5cc9
darwish1998 <darwish144.ad@gmail.com>
2021-09-04 21:48:02 +0300

    Changes on static files

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -132,3 +132,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("There is local file, It must be production ")
